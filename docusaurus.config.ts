0797a5d0ae3a7193bc0b40b28060fe7bf8379b19
Silvan Flum <54156690+floomm@users.noreply.github.com>
2024-10-05 17:47:45 +0200

    Rename project to docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,12 +9,12 @@ const config: Config = {
   url: 'https://flum.tech',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/blog/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'floomm', // Usually your GitHub org/user name.
-  projectName: 'blog', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
